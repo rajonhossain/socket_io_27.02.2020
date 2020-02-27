c7bd2bfd6529a85509045dbe91c054a87be66fea
rajon hossain <rajonhossaindhaka@gmail.com>
2020-02-27 19:19:41 +0600

    sdfdsF

---
+++ htt.js
@@ -0,0 +1,15 @@
+
+let bd = new Promise((resolve, reject)=>{
+	let name = "bangladesh........";
+
+	resolve(name);
+})
+
+
+	bd
+	.then((name)=>{
+		console.log("..."+ name)
+})
+	.catch(()=>{
+
+	})
